de4bac8778adab22874e30c4b6fc6c6fc8970551
gerry_sucipto <thomassucipto88@gmail.com>
2018-04-25 09:01:37 +0700

    hampir jaedi1

--- app/src/main/java/com/example/gerrys/charperone/ActivityFront.java
+++ app/src/main/java/com/example/gerrys/charperone/ActivityFront.java
@@ -1,33 +1,35 @@
 package com.example.gerrys.charperone;
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class FrontActivity extends AppCompatActivity {
+public class ActivityFront extends AppCompatActivity
+        {
 
-    Button btnSignUp, btnSignIn;
-    TextView txtSlogan;
-    ImageView image ;
-
+            Button btnSignUp, btnSignIn;
+            TextView txtSlogan;
+            ImageView image ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ss);
+
         image = (ImageView) findViewById(R.id.logob);
         btnSignUp = (Button)findViewById(R.id.btnSignUp);
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
-        image.setImageDrawable(getDrawable(R.drawable.logob));
+        image.setImageDrawable(getDrawable(R.drawable.ic_arrow_back_black_24dp));
 
 
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FrontActivity.this, SignIn.class);
+                Intent intent = new Intent(ActivityFront.this, SignIn.class);
                 startActivity(intent);
             }
         });
@@ -35,9 +37,13 @@ public class FrontActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FrontActivity.this, SignUp.class);
+                Intent intent = new Intent(ActivityFront.this, SignUp.class);
                 startActivity(intent);
             }
         });
+    }
 
-    }}
+
+
+
+}
